8909ec8d0e92c7cc14b6d6167dd36d6a5c7160d0
Kristopher Williams <kwilliams@roarmedia.com>
2018-07-19 12:26:20 -0400

    Initial commit.

---
+++ rm-popup.php
@@ -0,0 +1,7 @@
+<?php
+	Plugin Name: Roar Media Pop-ups
+	Description: A plugin that extends the Roar Media theme for additional popup functionality.
+	Version: 0.0.1
+	Author: Roar Media, Kris Williams
+	Author URI: mailto:webmaster@roarmedia.com
+?>
